c506d379aed270d034dbef4cb30ab49baaede9e3
Zetille03 <ricardogarciaromera1@gmail.com>
2023-12-14 10:53:50 +0100

    commitdiciembre

--- app/src/main/java/com/example/bussinesscard/MainActivity.kt
+++ app/src/main/java/com/example/bussinesscard/MainActivity.kt
@@ -3,12 +3,18 @@ package com.example.bussinesscard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.AlertDialogDefaults.shape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -16,8 +22,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.bussinesscard.ui.theme.BussinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,7 +37,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.secondary
                 ) {
                     Display()
                 }
@@ -40,18 +49,83 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Display(modifier: Modifier = Modifier) {
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally
-        //verticalArrangement = Arrangement.
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .background(Color(0xFF073042)),
     ){
-        Text(
-            text = "hola",
-
+        Image(
+            painter = painterResource(R.drawable.android_logo),
+            contentDescription = null,
+            modifier = Modifier
+                .height(130.dp)
+                .width(160.dp)
         )
-        Text(
-            text = "hola"
-        )
+        Text(text = "Ricardo Garcia", color = Color.White, fontSize = 32.sp)
+        Text(text = "Senior Android Developer", color = Color.Green)
     }
 
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 50.dp),
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.Start
+    ){
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 50.dp)
+        ){
+            val tlf = painterResource(R.drawable.ic_baseline_local_phone_24)
+            Image(
+                painter = tlf,
+                contentDescription = null,
+                modifier = Modifier.padding(10.dp)
+            )
+            Text(
+                text = "+34 698 90 78 43",
+                color = Color.White,
+                modifier = Modifier.padding(10.dp)
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 50.dp)
+        ){
+            val compartir = painterResource(R.drawable.ic_baseline_share_24)
+            Image(
+                painter = compartir,
+                contentDescription = null,
+                modifier = Modifier.padding(10.dp)
+            )
+            Text(
+                text = "@RicardoDEV03",
+                color = Color.White,
+                modifier = Modifier.padding(10.dp)
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 50.dp)
+        ){
+            val email = painterResource(R.drawable.ic_baseline_email_24)
+            Image(
+                painter = email,
+                contentDescription = null,
+                modifier = Modifier.padding(10.dp)
+            )
+            Text(
+                text = "ricardogarciaromera1@gmail.com",
+                color = Color.White,
+                modifier = Modifier.padding(10.dp)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
